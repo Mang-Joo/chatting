23f79e4ef8cd3d10962f792ad85d32e2bf53352c
kimwanjoo <wjwan0@hanpass.com>
2023-06-13 17:55:12 +0900

    :sparkles: feat(gradle): add logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.github.microutils:kotlin-logging:3.0.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
